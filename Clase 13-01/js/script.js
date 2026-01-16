af29dc33913c21bf3c3456be44ccfc8741c609e7
paulaalbornozra <paula.albornoz.ra@gmail.com>
2026-01-15 21:15:07 -0300

    Primera carga de archivos

---
+++ Clase 13-01/js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+//clave
+    $("#togglePassword").on("click", function(){
+        const passwordField = $("password");
+        const icon = $(this).find(i);
+
+        if (passwordField.attr("type")=== "password"){
+            passwordField.attr("type", text);
+        }
+
+
+
+    });
+
+
+
+});
